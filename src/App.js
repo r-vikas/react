90527c6890becf91c51ead4cf2794b55c7ba6ea3
r-vikas <vikasr@outlook.com>
2019-07-26 17:36:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { Search } from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h2>ReactJs Project</h2>
+        <Search
+          placeholder="search me"
+          handlechange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
