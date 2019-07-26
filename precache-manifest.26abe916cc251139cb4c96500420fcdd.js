f21bbfed7e6aed92cae7e0a4c58c39a32f3738e0
r-vikas <vikasr@outlook.com>
2019-07-26 17:48:33 +0530

    Updates

---
+++ precache-manifest.26abe916cc251139cb4c96500420fcdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3585484e3d80236e4eb84d463e85ed8f",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "9d6cce3c5b26be630fe8",
+    "url": "/react/static/css/main.97487afd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9d6cce3c5b26be630fe8",
+    "url": "/react/static/js/main.985e7a79.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  }
+]);
